6371da7e1b3c0fb59da22e380a789af572373ea0
Александра Дюпина <adiupina@astralinux.ru>
2024-11-27 23:19:26 +0300

    1st increment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,66 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"net/http"
+
+	"github.com/9ssi7/nanoid"
+)
+
+var urlStore = make(map[string]string)
+
+func generateShortID() string {
+	id, _ := nanoid.New()
+	return id
+}
+
+// curl -X POST http://localhost:8080 -H "Content-Type: text/plain" -d "https://example.com"
+func shortenURL(res http.ResponseWriter, req *http.Request) {
+	// этот обработчик принимает только запросы, отправленные методом POST
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, _ := io.ReadAll(req.Body) // тело запроса
+	originalURL := string(body)
+
+	shortID := generateShortID()
+
+	urlStore[shortID] = originalURL
+
+	shortURL := "http://localhost:8080/" + shortID
+	res.WriteHeader(http.StatusCreated)
+	res.Write([]byte(shortURL))
+}
+
+func getOriginalURL(res http.ResponseWriter, req *http.Request) {
+	// этот обработчик принимает только запросы, отправленные методом GET
+	if req.Method != http.MethodGet {
+		http.Error(res, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := req.PathValue("id") // получить id из /{id}
+
+	originalURL, exists := urlStore[id]
+
+	if !exists {
+		http.Error(res, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(res, req, originalURL, http.StatusTemporaryRedirect)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", shortenURL)
+	mux.HandleFunc("/{id}", getOriginalURL)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
