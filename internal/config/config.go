b13eb7db030daa6ad8d68895175de64234ec925d
Александра Дюпина <adiupina@astralinux.ru>
2024-11-29 19:00:12 +0300

    4th increment

---
+++ internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"errors"
+	"flag"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+var Addr *NetAddress
+
+type NetAddress struct {
+	Host    string
+	Port    int
+	BaseURL string
+}
+
+func Init() {
+	addr := &NetAddress{ // значения по-умолчанию
+		Host:    "localhost",
+		Port:    8080,
+		BaseURL: "http://localhost:8080",
+	}
+
+	// декларируем функцию-обработчик
+	flag.Func("a", "адрес запуска HTTP-сервера host:port", func(flagValue string) error {
+		hp := strings.Split(flagValue, ":")
+		if len(hp) != 2 {
+			return errors.New("need address in a form host:port")
+		}
+		port, err := strconv.Atoi(hp[1])
+		if err != nil {
+			return err
+		}
+		addr.Host = hp[0]
+		addr.Port = port
+		return nil
+	})
+
+	flag.Func("b", "базовый адрес результирующего сокращённого URL", func(flagValue string) error {
+		if _, err := url.ParseRequestURI(flagValue); err != nil {
+			return errors.New("need a valid url")
+		}
+		addr.BaseURL = flagValue
+		return nil
+	})
+
+	// запускаем парсинг
+	flag.Parse()
+
+	// fmt.Println(addr.Host)
+	// fmt.Println(addr.Port)
+	// fmt.Println(addr.BaseURL)
+
+	Addr = addr
+}
